be49fafcdaf665b0d61618b3ac522869e3250bcb
George Mihov <gmihov001@gmail.com>
2020-07-23 20:01:18 +0000

    Updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,32 @@
-/* eslint-disable */
+window.onload = () => {
+  var parent = document.querySelector(".container");
 
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
-import "../style/index.scss";
+  for (var i = 0; i < 10; i++) {
+    var row = document.createElement("div");
+    parent.appendChild(row);
+    row.style.display = "flex";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+    for (var j = 0; j < 10; j++) {
+      var cell = document.createElement("div");
+      row.appendChild(cell);
+      cell.innerHTML = Math.floor(Math.random() * 2);
+      cell.style.border = "1px solid grey";
+      cell.style.padding = "15px";
+      if (cell.innerHTML == 1) cell.style.background = "gray";
+    }
+  }
+
+  const fire = () => {
+    var fired =
+      parent.childNodes[Math.floor(Math.random() * 10)].childNodes[
+        Math.floor(Math.random() * 10)
+      ];
+    fired.style.background = "red";
+  };
+
+  var button = document.createElement("button");
+  button.innerHTML = "FIRE!";
+  button.style.background = "red";
+  parent.appendChild(button);
+  button.addEventListener("click", fire);
 };
